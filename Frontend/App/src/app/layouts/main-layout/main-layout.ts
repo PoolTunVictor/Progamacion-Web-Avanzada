b012cd6224e8babbd5edc6999f9f2384aae652d6
AzielHC04 <Aziel.Hidalgo110@gmail.com>
2026-04-05 21:03:07 -0600

    Correccion del escafolding y creacion de header y footer

---
+++ Frontend/App/src/app/layouts/main-layout/main-layout.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../shared/header/header';
+import { FooterComponent } from '../../shared/footer/footer';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.css'
+})
+export class MainLayoutComponent {}
