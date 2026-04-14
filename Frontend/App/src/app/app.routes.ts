202280cc5e907deca84fb77c5ac24360bc4cb3e9
Ashly <nayibeuc@gmail.com>
2026-04-14 10:18:11 -0600

    pagina contacto

--- Frontend/App/src/app/app.routes.ts
+++ Frontend/App/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout';
+// Cambia la ruta a esta (quitamos el ".component"):
+import { ContactoComponent } from './pages/contacto/contacto'; 
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    children: []
+    children: [
+      { path: 'contacto', component: ContactoComponent }
+    ]
   }
 ];
