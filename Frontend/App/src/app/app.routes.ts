11c9be5a49fc68bc1d0b9844e6c100b2a5ce3c56
PoolTunVictor <victorgamer738@gmail.com>
2026-04-14 09:48:27 -0600

    Page - Map

--- Frontend/App/src/app/app.routes.ts
+++ Frontend/App/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout';
 import { Directory } from './pages/directory/directory';
+import { Map } from './pages/map/map';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
     children: [
-      { path: 'directorio', component: Directory }
+      { path: 'directorio', component: Directory },
+      { path: 'mapa', component: Map }
     ]
   }
 ];
